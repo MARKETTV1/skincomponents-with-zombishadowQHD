1345a4014896bd5ebea2513de1830d9b4a8c42ef
MARKETTV1 <aliridakarim8@gmail.com>
2025-10-30 23:53:27 +0100

    Add readme.sh file

---
+++ readme.sh
@@ -0,0 +1,15 @@
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extaudioinfo_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extcaidinfo_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extchnumber_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extconverterrotator_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extdiskspaceinfo_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extmovieinfo_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extmultilistselection_all.deb
+
+https://github.com/MARKETTV1/skincomponents-with-zombishadowQHD/raw/refs/heads/main/enigma2-plugin-skincomponents-extvolumetext_all.deb
